e0e9c0b355f0bca4797cda8f286e061e0e7452a7
Chris Iverach-Brereton <civerachb@clearpathrobotics.com>
2022-10-14 15:32:37 -0400

    Update the datum scripts to use the new envars

--- cpr_agriculture_gazebo/scripts/datum.bash
+++ cpr_agriculture_gazebo/scripts/datum.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-# Used for OutdoorNav simulation to set the datum
-export DATUM_LAT=50.109461
-export DATUM_LON=-97.318978
+export GAZEBO_WORLD_LAT=50.109461
+export GAZEBO_WORLD_LON=-97.318978
--- cpr_orchard_gazebo/scripts/datum.bash
+++ cpr_orchard_gazebo/scripts/datum.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-# Used for OutdoorNav simulation to set the datum
-export DATUM_LAT=39.512152
-export DATUM_LON=22.426669
+export GAZEBO_WORLD_LAT=39.512152
+export GAZEBO_WORLD_LON=22.426669
